b02a9e2baec857f63c1821f9c2ec8300b614dacf
ana-florescu <florescualexandra18@stud.ase.ro>
2021-03-25 14:30:25 +0200

    before the btreak

---
+++ Week 5 - Singleton/src/ro/ase/csie/cts/g1093/dp/singleton/TestGame.java
@@ -0,0 +1,14 @@
+package ro.ase.csie.cts.g1093.dp.singleton;
+
+public class TestGame {
+
+	public static void main(String[] args) {
+		RESTBackend backend =  RESTBackend.getRESTBackend();
+		
+		UIModule uiModule = new UIModule();
+		PlayerModule playerModule = new PlayerModule();
+		
+
+	}
+
+}
---
+++ Week 5 - Singleton/src/ro/ase/csie/cts/g1093/dp/singleton/UIModule.java
@@ -0,0 +1,9 @@
+package ro.ase.csie.cts.g1093.dp.singleton;
+
+public class UIModule {
+
+	public UIModule() {
+		RESTBackend backend =  RESTBackend.getRESTBackend();
+		
+	}
+}
