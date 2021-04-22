6d4605d23e02994f1c883b3a198b68e9e9fc8886
ana-florescu <florescualexandra18@stud.ase.ro>
2021-04-22 15:19:32 +0300

    after the break - Flyweight

---
+++ Week 9 - Composite Flyweight/src/ro/ase/csie/cts/g1093/dp/flyweight/ScreenData.java
@@ -0,0 +1,16 @@
+package ro.ase.csie.cts.g1093.dp.flyweight;
+
+public class ScreenData {
+
+	int x,z,y;
+	String textureColor;
+	public ScreenData(int x, int z, int y, String textureColor) {
+		super();
+		this.x = x;
+		this.z = z;
+		this.y = y;
+		this.textureColor = textureColor;
+	}
+	
+	
+}
